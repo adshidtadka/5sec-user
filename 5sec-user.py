1923bf745d93178b3ed154c8c11f3a9412dd9744
adshidtadka <nextage900fx@gmail.com>
2019-11-25 16:34:50 +0900

    create stop screen

--- 5sec-user.py
+++ 5sec-user.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+user_name = "user 1"
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="5sec stopwatch")
+    return render_template('index.html', title="5sec stopwatch", user_name=user_name)
+
+
+@app.route('/play')
+def play():
+    return render_template('play.html', title="5sec stopwatch | play", user_name=user_name)
 
 
 if __name__ == '__main__':
