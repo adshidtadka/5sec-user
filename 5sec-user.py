f0adef395ef11bdb0bd2b314dd88fc637c05a1c3
adshidtadka <nextage900fx@gmail.com>
2019-11-26 10:20:51 +0900

    viewでカウントダウンを完結させる

--- 5sec-user.py
+++ 5sec-user.py
@@ -1,5 +1,5 @@
 import time
-from datetime import date
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -14,9 +14,7 @@ def index():
 
 @app.route('/play')
 def play():
-    start_time = date.today()
-    start_time_str = start_time.strftime("%Y/%m/%d %H:%M:%S")
-    return render_template('play.html', title="5sec stopwatch | play", user_name=user_name, start_time=start_time_str)
+    return render_template('play.html', title="5sec stopwatch | play", user_name=user_name)
 
 
 if __name__ == '__main__':
--- static/js/countDown.js
+++ static/js/countDown.js
@@ -1,16 +1,21 @@
 $(function() {
+  getEndTime();
   countDown();
 });
 
-const countDown = function() {
-  var startTime = new Date();
-  //カウントダウンの終了期日を記入↓
-  var endTime = new Date(startTime.getTime());
+let endTime;
+
+const getEndTime = function() {
+  endTime = new Date();
   endTime.setSeconds(endTime.getSeconds() + 5);
-  var diff = endTime - startTime;
-  var times = 24 * 60 * 60 * 1000;
-  var sec = (Math.floor((diff % times) / 1000) % 60) % 60;
-  var ms = Math.floor((diff % times) / 10) % 100;
+};
+
+const countDown = function() {
+  const startTime = new Date();
+  const diff = endTime - startTime;
+  const times = 24 * 60 * 60 * 1000;
+  const sec = (Math.floor((diff % times) / 1000) % 60) % 60;
+  const ms = Math.floor((diff % times) / 10) % 100;
   if (diff > 0) {
     $("#timer").text(sec + "秒" + ms);
     setTimeout(countDown, 10);
