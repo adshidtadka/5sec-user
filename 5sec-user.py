a8ca26c2c5c99dc703d3994706cb03ecf4e10dcd
adshidtadka <nextage900fx@gmail.com>
2019-11-28 13:32:31 +0900

    コマンドラインからuserをいいかんじに

--- 5sec-user.py
+++ 5sec-user.py
@@ -2,19 +2,23 @@ import time
 import datetime
 from flask import Flask, render_template
 import configparser
+import sys
+
+args = sys.argv
+# config = configparser.ConfigParser()
+# config.read("config.ini")
+
+DEBUG = False
+SERVER_NAME = "localhost:" + str(5000 + int(sys.argv[1]))
 
 app = Flask(__name__)
-
-user_name = "user 1"
+app.config.from_object(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="5sec stopwatch", user_name=user_name)
+    return render_template('index.html', title="5sec stopwatch", user_name="user " + sys.argv[1])
 
 
 if __name__ == '__main__':
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    port = config["DEFAULT"]["port"]
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run()
