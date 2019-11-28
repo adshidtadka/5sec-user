b4c927d1f876a808d9b3e996d611d37023be6aea
adshidtadka <nextage900fx@gmail.com>
2019-11-28 13:48:03 +0900

    faviconをちゃんとする

--- 5sec-user.py
+++ 5sec-user.py
@@ -1,8 +1,9 @@
 import time
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import configparser
 import sys
+import os
 
 args = sys.argv
 # config = configparser.ConfigParser()
@@ -13,6 +14,11 @@ SERVER_NAME = "localhost:" + str(5000 + int(sys.argv[1]))
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static/img'), 'stopwatch.png')
 
 
 @app.route('/')
