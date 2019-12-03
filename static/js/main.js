cda3451626f7433557e2e9f5fa12587d7646d336
adshidtadka <nextage900fx@gmail.com>
2019-12-03 15:32:55 +0900

    ガウス関数を作る

--- static/js/main.js
+++ static/js/main.js
@@ -12,6 +12,8 @@ let timerTimeOut, getPlayersTimeOut;
 let fetchedPlayers = [];
 
 const startLoading = function() {
+  const x = gaussian(0.5, 0.5)();
+  console.log(x);
   $("#table-thead").append("<th scope='col'>#</th><th scope='col'>user</th>");
   loading(LOADING);
   getPlayers(fetchedPlayers);
@@ -153,4 +155,30 @@ const stopTimer = function(e) {
     .always(data => {
       console.log(data);
     });
+};
+
+// returns a gaussian random function with the given mean and stdev.
+const gaussian = function(mean, stdev) {
+  var y2;
+  var use_last = false;
+  return function() {
+    var y1;
+    if (use_last) {
+      y1 = y2;
+      use_last = false;
+    } else {
+      var x1, x2, w;
+      do {
+        x1 = 2.0 * Math.random() - 1.0;
+        x2 = 2.0 * Math.random() - 1.0;
+        w = x1 * x1 + x2 * x2;
+      } while (w >= 1.0);
+      w = Math.sqrt((-2.0 * Math.log(w)) / w);
+      y1 = x1 * w;
+      y2 = x2 * w;
+      use_last = true;
+    }
+
+    return mean + stdev * y1;
+  };
 };
