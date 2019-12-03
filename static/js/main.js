71ac54f4e97abfca3e112fa6412fb5e08a5d9395
adshidtadka <nextage900fx@gmail.com>
2019-12-04 02:41:45 +0900

    1/100秒に揃える

--- static/js/main.js
+++ static/js/main.js
@@ -41,7 +41,7 @@ const joinGame = function() {
 };
 
 const generateRandomScore = function() {
-  const randomTime = Math.round(gaussian(0.5, 0.5)() * 1000);
+  const randomTime = gaussian(50, 50)();
   const intervalTime =
     getRemainTime(endLoadingTime).time +
     (COUNTDOWN + TIMER) * 1000 -
